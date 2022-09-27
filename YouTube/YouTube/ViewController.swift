373aebe017087916119c573c404dfe972c4f91b1
yongunLim <dyddns1906@gmail.com>
2022-09-27 19:41:33 +0900

    init project

---
+++ YouTube/YouTube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTube
+//
+//  Created by Yongun Lim on 2022/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
