d40c9662413a925468d42266ec31a90e2751d9a1
yongunLim <dyddns1906@gmail.com>
2022-10-08 15:44:09 +0900

    home 리스트 비디오 셀 UI 작업

--- YouTube/View/Common/BaseVideoItemView.swift
+++ YouTube/View/Common/BaseVideoItemView.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class BaseVideoItemView: BaseViewForXib {
 
+    @IBOutlet weak var thumbnailView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var moreButton: UIButton!
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
---
+++ YouTube/View/Home/Cell/HomeVideoTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  HomeVideoTableViewCell.swift
+//  YouTube
+//
+//  Created by Yongun Lim on 2022/10/08.
+//
+
+import UIKit
+
+class HomeVideoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var videoView: BaseVideoItemView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
